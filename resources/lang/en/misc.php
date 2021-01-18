14353141a097e19838ba960be02dfc8d7784e4e8
kalvo19 <recmanvideos@gmail.com>
2021-01-18 14:51:36 +0100

    [UPDATE] Upgrade Google Captcha v2 to v3
    Spam and automated queries were notoriously sent in the contact form inside the footer, receiving roughly between 20 and 25 emails per day. As to prevent this to happen the Google Captcha was upgrated from v2 to v3 which no longer uses a checkbox to verify but a token instead.
    
    - Modified Rules for Captcha
    - Displayed Google Captcha branding inside the form
    - Hidden Google Captcha badge

---
+++ resources/lang/en/misc.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'recaptcha-branding' =>     '<p style="font-size::font_size">This site is protected by reCAPTCHA and the Google ' .
+                                    '<a href="https://policies.google.com/privacy">Privacy Policy</a> and
+                                    <a href="https://policies.google.com/terms">Terms of Service</a> apply.
+                                </p>'
+];
