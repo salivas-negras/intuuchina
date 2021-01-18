22712da6918092fc0b9e0038a99e8da7959b44ec
kalvo19 <recmanvideos@gmail.com>
2021-01-18 16:43:15 +0100

    [FIX] Point the correct signature translation.

--- resources/views/components/misc.blade.php
+++ resources/views/components/misc.blade.php
@@ -3,7 +3,7 @@
         <p>{!! __('misc.signature', ['year' => \Carbon\Carbon::now()->year]) !!}</p>
     </section>
     <section class="misc__author">
-        <span>{{ __('content.made with love by ') }}</span>
+        <span>{{ __('misc.made with love by ') }}</span>
         <a href="http://factoriaf5.org"><img src="{{ asset('storage/images/logo_factoriaf5.png') }}" alt="Logo de factoriaF5"></a>
     </section>
     <section class="misc__legal">
